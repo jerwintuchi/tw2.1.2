fc7a317da2d5b0bfe5ec92a59f70b28805306d0b
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-02-10 13:23:33 +0800

    added helper function to retrieve username along with supabase user object

--- utils/supabase/user-helpers-server.ts
+++ utils/supabase/user-helpers-server.ts
@@ -1,3 +1,4 @@
+import { UserWithUsername } from "@/app/types/type-definitions";
 import { createClient } from "./server";
 
 export const getUser = async () => {
@@ -7,4 +8,30 @@ export const getUser = async () => {
   } = await supabase.auth.getUser();
 
   return user;
+};
+
+export const getUserName = async (userId: string) => {
+  const supabase = await createClient();
+
+  const username = await supabase
+    .from("profiles")
+    .select("username")
+    .eq("user_id", userId)
+    .single();
+  if (!username.data) {
+    console.log("Cannot find username");
+    return null;
+  }
+
+  return username.data?.username;
+};
+
+export const getUserWithUsername = async () => {
+  const user = await getUser();
+  if (!user) return null;
+
+  const username = await getUserName(user.id);
+  if (!username) return null;
+
+  return { supabaseUser: user, username: username } as UserWithUsername;
 };
