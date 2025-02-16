835cc140a7ceac6af204e29ee25f5322768ca2c1
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-02-16 13:35:18 +0800

    removed exclude and updated imports from tests

--- __tests__/frontend/(auth)/FormMessage.test.ts
+++ __tests__/frontend/(auth)/FormMessage.test.ts
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { FormMessage } from "../../../components/form-message";
+import { FormMessage } from "@/components/form-message";
 import "@testing-library/jest-dom/vitest";
 import { describe, it, expect } from "vitest";
 
