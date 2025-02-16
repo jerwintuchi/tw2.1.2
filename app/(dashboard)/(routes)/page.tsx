98a0fc4338c5c86ff884695973b7c953635d9fa1
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-02-16 13:30:30 +0800

    deleted unnecessary imports

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,7 @@
 
-import { createClient } from "@/utils/supabase/server";
 import HomePage from "./_components/HomePage";
 import LandingPage from "./_components/LandingPage";
-import {  getUserWithUsername } from "@/utils/supabase/user-helpers-server";
+import { getUserWithUsername } from "@/utils/supabase/user-helpers-server";
 
 export default async function Root() {
   const user = await getUserWithUsername();
